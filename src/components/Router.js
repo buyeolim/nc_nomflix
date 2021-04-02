032914153c6023f9cb548e3ff36edca461dd3d98
buyeolim <buyeolim@gmail.com>
2021-04-02 23:27:23 +0900

    Add Header

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,15 +1,22 @@
 import React from "react";
-import { HashRouter as Router, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from "react-router-dom";
 import Home from "../routes/Home";
 import Tv from "../routes/Tv";
 import Search from "../routes/Search";
 
 export default () => (
   <Router>
-    <>
+    <Switch>
       <Route path="/" exact component={Home} />
       <Route path="/tv" exact component={Tv} />
-      <Route path="/search" exact component={Search} />
-    </>
+      <Route path="/tv/popular" render={() => <h1>Popular</h1>} />
+      <Route path="/search" component={Search} />
+      <Redirect from="*" to="/" />
+    </Switch>
   </Router>
 );
