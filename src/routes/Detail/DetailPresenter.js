6f53b6976f1bb2ad7219c2cd6c1dea63174838b1
buyeolim <buyeolim@gmail.com>
2021-05-13 15:42:19 +0900

    Update detail screen about movies and tv shows(ch.6.8-9)

--- src/routes/Detail/DetailPresenter.js
+++ src/routes/Detail/DetailPresenter.js
@@ -1,8 +1,112 @@
 import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
+import Loader from "../../components/Loader";
 
-const DetailPresenter = ({ result, error, loading }) => null;
+const Container = styled.div`
+  height: calc(100vh - 55px);
+  width: 100%;
+  padding: 50px;
+  position: relative;
+`;
+const Backdrop = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-image: url(${(props) => props.bgImage});
+  background-position: center center;
+  background-size: cover;
+  filter: blur(5px) grayscale(1);
+  opacity: 0.2;
+`;
+const Content = styled.div`
+  display: flex;
+  width: 100%;
+  height: 100%;
+  position: relative;
+  z-index: 1;
+`;
+const Cover = styled.div`
+  width: 30%;
+  height: 100%;
+  background-image: url(${(props) => props.bgImage});
+  background-position: center center;
+  background-size: cover;
+  border-radius: 5px;
+  z-index: 0;
+`;
+const Data = styled.div`
+  width: 70%;
+  margin-left: 25px;
+`;
+const Title = styled.span`
+  font-size: 30px;
+`;
+const ItemContainer = styled.div`
+  margin: 12px 0;
+`;
+const Item = styled.span`
+  font-size: 15px;
+  opacity: 0.5;
+`;
+const Divider = styled.span`
+  margin: 0 6px;
+  opacity: 0.5;
+`;
+const Overview = styled.p`
+  font-size: 20px;
+  opacity: 0.7;
+  line-height: 1.3;
+  width: 75%;
+`;
+
+const DetailPresenter = ({ result, error, loading }) =>
+  loading ? (
+    <Loader />
+  ) : (
+    <Container>
+      <Backdrop
+        bgImage={`https://image.tmdb.org/t/p/original${result.backdrop_path}`}
+      />
+      <Content>
+        <Cover
+          bgImage={
+            result.poster_path
+              ? `https://image.tmdb.org/t/p/original${result.poster_path}`
+              : require("../../assets/noPoster.png").default
+          }
+        />
+        <Data>
+          <Title>{result.title ? result.title : result.name}</Title>
+          <ItemContainer>
+            <Item>
+              {result.release_date
+                ? result.release_date.substring(0, 4)
+                : result.first_air_date.substring(0, 4)}
+            </Item>
+            <Divider>•</Divider>
+            <Item>
+              {result.runtime
+                ? `${result.runtime} min`
+                : `EP 1 - ${result.number_of_episodes} (${result.episode_run_time[0]} min)`}
+            </Item>
+            <Divider>•</Divider>
+            <Item>
+              {result.genres &&
+                result.genres.map((genre, index) =>
+                  index === result.genres.length - 1
+                    ? genre.name
+                    : `${genre.name} / `
+                )}
+            </Item>
+          </ItemContainer>
+          <Overview>{result.overview}</Overview>
+        </Data>
+      </Content>
+    </Container>
+  );
 
 DetailPresenter.propTypes = {
   result: PropTypes.object,
