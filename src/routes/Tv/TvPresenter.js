1ac290556b384331262c26ec76ae88c2d3acc83a
buyeolim <buyeolim@gmail.com>
2021-05-13 16:24:59 +0900

    Modify head title by pages(ch.6.10)

--- src/routes/Tv/TvPresenter.js
+++ src/routes/Tv/TvPresenter.js
@@ -1,6 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
+import Helmet from "react-helmet";
 import Section from "../../components/Section";
 import Loader from "../../components/Loader";
 import Message from "../../components/Message";
@@ -10,56 +11,62 @@ const Container = styled.div`
   padding: 0px 10px;
 `;
 
-const TvPresenter = ({ topRated, popular, airingToday, error, loading }) =>
-  loading ? (
-    <Loader />
-  ) : (
-    <Container>
-      {topRated && topRated.length > 0 && (
-        <Section title="Top Rated Shows">
-          {topRated.map((tv) => (
-            <Poster
-              key={tv.id}
-              id={tv.id}
-              title={tv.name}
-              imageUrl={tv.poster_path}
-              rating={tv.vote_average}
-              year={tv.first_air_date && tv.first_air_date.substring(0, 4)}
-            />
-          ))}
-        </Section>
-      )}
-      {popular && popular.length > 0 && (
-        <Section title="Popular Shows">
-          {popular.map((tv) => (
-            <Poster
-              key={tv.id}
-              id={tv.id}
-              title={tv.name}
-              imageUrl={tv.poster_path}
-              rating={tv.vote_average}
-              year={tv.first_air_date && tv.first_air_date.substring(0, 4)}
-            />
-          ))}
-        </Section>
-      )}
-      {airingToday && airingToday.length > 0 && (
-        <Section title="Airing Today Shows">
-          {airingToday.map((tv) => (
-            <Poster
-              key={tv.id}
-              id={tv.id}
-              title={tv.name}
-              imageUrl={tv.poster_path}
-              rating={tv.vote_average}
-              year={tv.first_air_date && tv.first_air_date.substring(0, 4)}
-            />
-          ))}
-        </Section>
-      )}
-      {error && <Message color="#e74c3c" text={error} />}
-    </Container>
-  );
+const TvPresenter = ({ topRated, popular, airingToday, error, loading }) => (
+  <>
+    <Helmet>
+      <title>TV shows | Nomflix</title>
+    </Helmet>
+    {loading ? (
+      <Loader />
+    ) : (
+      <Container>
+        {topRated && topRated.length > 0 && (
+          <Section title="Top Rated Shows">
+            {topRated.map((tv) => (
+              <Poster
+                key={tv.id}
+                id={tv.id}
+                title={tv.name}
+                imageUrl={tv.poster_path}
+                rating={tv.vote_average}
+                year={tv.first_air_date && tv.first_air_date.substring(0, 4)}
+              />
+            ))}
+          </Section>
+        )}
+        {popular && popular.length > 0 && (
+          <Section title="Popular Shows">
+            {popular.map((tv) => (
+              <Poster
+                key={tv.id}
+                id={tv.id}
+                title={tv.name}
+                imageUrl={tv.poster_path}
+                rating={tv.vote_average}
+                year={tv.first_air_date && tv.first_air_date.substring(0, 4)}
+              />
+            ))}
+          </Section>
+        )}
+        {airingToday && airingToday.length > 0 && (
+          <Section title="Airing Today Shows">
+            {airingToday.map((tv) => (
+              <Poster
+                key={tv.id}
+                id={tv.id}
+                title={tv.name}
+                imageUrl={tv.poster_path}
+                rating={tv.vote_average}
+                year={tv.first_air_date && tv.first_air_date.substring(0, 4)}
+              />
+            ))}
+          </Section>
+        )}
+        {error && <Message color="#e74c3c" text={error} />}
+      </Container>
+    )}
+  </>
+);
 
 TvPresenter.propTypes = {
   topRated: PropTypes.array,
